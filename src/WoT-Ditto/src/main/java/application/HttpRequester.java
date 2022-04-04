ba310f7d65e0e2924bf604f944210cf1ff075dcd
Alessandro <trofui@gmail.com>
2022-04-04 22:57:41 +0200

    wotditto taking shape

--- src/WoT-Ditto/src/main/java/application/HttpRequester.java
+++ src/WoT-Ditto/src/main/java/application/HttpRequester.java
@@ -1,15 +1,18 @@
 package application;
 
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Base64;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.javatuples.Pair;
 
@@ -17,129 +20,94 @@ import com.damnhandy.uri.template.UriTemplate;
 
 public class HttpRequester {
 
-	private final String usernameColonPassword = "ditto:ditto";
-    private final String basicAuthPayload = "Basic " + Base64.getEncoder().encodeToString(usernameColonPassword.getBytes());
-    private final String payloadThingCreation = "{\n"
-            + "    \"definition\": \"https://raw.githubusercontent.com/aleshark87/WoTModels/main/lamp/Lamp.jsonld\"\n"
-            + "}";
+    private final String basicAuthPayload = "Basic " + Base64.getEncoder().encodeToString(("ditto:ditto").getBytes());
     private HttpClient client;
     private ThingDescription thingDescript;
+    private List<Map<String, Object>> uriVariables;
+    private String uriBase = "http://localhost:8080/api/2/things/projects.wot.ditto:lamp";
     
     public HttpRequester() {
         client = HttpClient.newHttpClient();
-        getThing();
-        List<Pair<String, String>> headersList = new LinkedList<>(); 
-        headersList.add(Pair.with("Accept", "application/td+json"));
-        //headersList.add(Pair.with("Authorization", basicAuthPayload));
-        
-        System.out.println(makeHttpRequest(
-        		"https://raw.githubusercontent.com/aleshark87/WoTModels/main/.tm.jsonld", headersList, "GET"));
+        //System.out.println(createThing());
+        getThingDescription();
+        setSerialNumber();
     }
     
+    public int createThing() {
+    	List<Pair<String, String>> headersList = 
+    				Stream.of(Pair.with("Content-Type", "application/json"), Pair.with("Authorization", basicAuthPayload))
+    				.collect(Collectors.toList());
+    	String body = "{\n"
+                + "    \"definition\": \"https://raw.githubusercontent.com/aleshark87/WoTModels/main/lamp/Lamp.jsonld\"\n"
+                + "}";
+        var response = makeHttpRequest(uriBase, false, headersList, "PUT", Optional.of(BodyPublishers.ofString(body)));
+        return response.getValue0();
+    }
     
-    public Pair<Integer, String> makeHttpRequest(String URI, List<Pair<String, String>> headersList, String requestType) {
+    public int getThingDescription() {
+    	List<Pair<String, String>> headersList = 
+				Stream.of(Pair.with("Accept", "application/td+json"), Pair.with("Authorization", basicAuthPayload))
+				.collect(Collectors.toList());
+    	var response = makeHttpRequest(uriBase, false, headersList, "GET", Optional.empty());
+    	
+    	thingDescript = new ThingDescription(response.getValue1());
+    	uriVariables = thingDescript.getURIVariables();
+    	return response.getValue0();
+    }
+    
+    public int setSerialNumber() {
+    	var endpoint = thingDescript.getAttributeEndPoint().get(1);
+    	
+    	return 0;
+    }
+    
+    private Pair<Integer, String> makeHttpRequest(String URI, boolean explodeURI, List<Pair<String, String>> headersList, String requestType, Optional<BodyPublisher> body) {
     	HttpResponse<String> response = null;
         try {
+        	if(explodeURI) {
+        		URI = explodeURI(URI);
+        	}
         	HttpRequest.Builder builder = HttpRequest.newBuilder().uri(new URI(URI));	
         	for(int i = 0; i < headersList.size(); i++) {
         		builder = builder.header(headersList.get(i).getValue0(), headersList.get(i).getValue1());
         	}
-        	HttpRequest req = builder.GET().build();
+        	switch(requestType) {
+        	  case "GET":
+        	    builder = builder.GET();
+        	    break;
+        	  case "PUT":
+        		if(body.isPresent()) {
+        			builder = builder.PUT(body.get());
+        		}
+        		else {
+        			System.out.println("PUT requests require body");
+        		}
+        	    break;
+        	  case "PATCH":
+        		  if(body.isPresent()) {
+        			  //https://stackoverflow.com/questions/58841919/java-11-httprequest-with-patch-method
+        			  //builder = builder.PA
+        		  }
+        	  default:
+        		  throw new Exception("Requesttype not found");
+        	}
+        	HttpRequest req = builder.build();
         	response = client.send(req, BodyHandlers.ofString());
         } catch(Exception e) {
-        	e.printStackTrace();
         	System.out.println("HttpRequest raised Exception.");
         	e.printStackTrace();
         }
         
         return Pair.with(response.statusCode(), response.body());
     }
-    public int createThing() {
-        int returnCode = -1;
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI("http://localhost:8080/api/2/things/projects.wot.ditto:lamp"))
-                    .headers("Content-Type", "application/json")
-                    .headers("Authorization", basicAuthPayload)
-                    .PUT(HttpRequest.BodyPublishers.ofString(payloadThingCreation))
-                    .build();
-            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            System.out.println(response);
-            returnCode = response.statusCode();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return returnCode;
-    }
-    
-    public void getThing() {
-    	int returnCode = -1;
-    	try {
-    		HttpRequest req = HttpRequest.newBuilder()
-    					.uri(new URI("http://localhost:8080/api/2/things/projects.wot.ditto:lamp"))
-    					.headers("Accept", "application/td+json")
-                        .headers("Authorization", basicAuthPayload)
-                        .GET()
-                        .build();
-    		HttpResponse<String> response = client.send(req, BodyHandlers.ofString());
-    		//System.out.println(response.body());
-    		thingDescript = new ThingDescription(response.body());
-    	} catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    }
-    
-    private HttpRequest getRequestGET(String endpoint) {
-    	HttpRequest req = null;
-    	try {
-			req = HttpRequest.newBuilder()
-					.uri(new URI("http://localhost:8080/api/2/things/projects.wot.ditto:lamp" + endpoint))
-					.headers("Accept", "application/json")
-			        .headers("Authorization", basicAuthPayload)
-			        .GET()
-			        .build();
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	return req;
-    }
     
     private String explodeURI(String uriToExplode) {
-    	String result = UriTemplate.fromTemplate("/attributes/serialNo{?channel,timeout}")
-    	                           .set("channel", 5)
-    	                           .set("timeout", 7)
-    	                           .expand();
-    	return result;
+    	UriTemplate template = UriTemplate.fromTemplate(uriToExplode);
+    	for(var uriVariable: uriVariables) {
+    		template = template.set(uriVariable);
+    	}
+    	return template.expand();
     }
-    
-    public void getAttribute() {
-    	System.out.println(explodeURI("cacca"));
-    	/*
-    	if (!(thingDescript == null)) {
-    		List<Pair<String,String>> endpoint = thingDescript.getAttributeEndPoint();
-        	System.out.println(endpoint);
-    	}*/
-    	
-    	
-    	/*
-    	HttpRequest req = getRequestGET(endpoint.get(0).getValue0().split("{")[0]);
-    	HttpResponse<String> response;
-		try {
-			response = client.send(req, BodyHandlers.ofString());
-			System.out.println(response);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
-    }
+   
     
 }
--- src/WoT-Ditto/src/main/java/application/ThingDescription.java
+++ src/WoT-Ditto/src/main/java/application/ThingDescription.java
@@ -1,8 +1,10 @@
 package application;
 
-import java.util.LinkedList;
+
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.javatuples.Pair;
 import org.json.JSONArray;
@@ -31,15 +33,29 @@ public class ThingDescription {
 		JSONArray opArray = jsonObj.getJSONObject("properties").getJSONObject("serialNo").getJSONArray("forms");
 		for (int i = 0; i < opArray.length(); i++) {
 			  JSONObject obj = opArray.getJSONObject(i);
-			  if(obj.getString("op").equals("readproperty")) {
-				  readEndPoints = Pair.with(obj.getString("href"), obj.getString("htv:methodName"));
-			  }
-			  if(obj.getString("op").equals("writeproperty")) {
-				  writeEndPoints = Pair.with(obj.getString("href"), obj.getString("htv:methodName"));
+			  try {
+				  if(obj.getString("op").equals("readproperty")) {
+					  readEndPoints = Pair.with(obj.getString("href"), obj.getString("htv:methodName"));
+				  }
+				  if(obj.getString("op").equals("writeproperty")) {
+					  writeEndPoints = Pair.with(obj.getString("href"), obj.getString("htv:methodName"));
+				  }
+			  } catch(JSONException je) {
+				  
 			  }
 		}
-		List<Pair<String, String>> list = new LinkedList<>();
-		list.add(readEndPoints); list.add(writeEndPoints);
-		return list;
+
+		return Stream.of(readEndPoints, writeEndPoints).collect(Collectors.toList());
+	}
+	
+	/**
+	 * 
+	 * @return List of URI Variables contained in the Description
+	 */
+	public List<Map<String, Object>> getURIVariables(){
+		Map<String, Object> channel = Map.of("channel", jsonObj.getJSONObject("uriVariables").getJSONObject("channel").getString("default"));
+		Map<String, Object> timeout = Map.of("timeout",Integer.toString(jsonObj.getJSONObject("uriVariables").getJSONObject("timeout").getInt("default")));
+		Map<String, Object> response_required = Map.of("response-required", jsonObj.getJSONObject("uriVariables").getJSONObject("response-required").getBoolean("default"));
+		return Stream.of(channel, timeout, response_required).collect(Collectors.toList());
 	}
 }
